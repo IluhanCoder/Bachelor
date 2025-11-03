763b2a2f187e073a155472963c780690e73b663e
Elijah <Elijah@MacBook-Air.local>
2025-11-03 18:52:38 +0200

    translated dates

--- client/src/misc/date-formatter.tsx
+++ client/src/misc/date-formatter.tsx
@@ -7,7 +7,7 @@ type LocalProps = {
 
 const DateFormater = (props: LocalProps) => {
   if (props.value) {
-    const dateFormat = "uk-UA";
+    const dateFormat = "en-US";
     const date = new Date(props.value);
     const dateOptions: any = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
     if (props.dayOfWeek) dateOptions.weekday = "long";
